b8bf642e98f6670e03bd1aa84ddd1619cc629ca4
Himanshu Pandey <himanshupandey.in@gmail.com>
2020-04-29 00:40:31 -0700

    Initial commit to save interview concept questions

---
+++ src/main/java/common/TreeNode.java
@@ -0,0 +1,21 @@
+package common;
+
+public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+    @Override
+    public String toString() {
+        return "TreeNode{" + "val=" + val + ", left=" + left + ", right=" + right + '}';
+    }
+
+    public TreeNode(int x) {
+        this.val = x;
+    }
+
+    public TreeNode(TreeNode left, int val, TreeNode right) {
+        this.left = left;
+        this.val = val;
+        this.right = right;
+    }
+}
